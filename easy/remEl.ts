05aa1f8ba744d219e38ebdfb9319e9d4525611f2
samin2668 <44666180+samin2668@users.noreply.github.com>
2023-08-07 14:58:23 -0500

    3 problems

--- easy/remEl.ts
+++ easy/remEl.ts
@@ -2,17 +2,24 @@
 function removeElement(nums: number[], val: number): number {
     let count = 0
 
-    let nextIndex = nums.length - 1
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] === val){
-            while(nums[nextIndex] === val){
-                nextIndex--
-            }
-                nums[i] = nums[nextIndex]
-                nextIndex--
-        }
-        else
+    // let nextIndex = nums.length - 1
+    // for(let i = 0; i < nums.length; i++){
+    //     if(nums[i] === val){
+    //         while(nums[nextIndex] === val){
+    //             nextIndex--
+    //         }
+    //             nums[i] = nums[nextIndex]
+    //             nextIndex--
+    //     }
+    //     else
+    //         count++
+    // }
+
+    for(let num of nums){
+        if(num !== val){
+            nums[count] = num
             count++
+        }
     }
 
     return count
